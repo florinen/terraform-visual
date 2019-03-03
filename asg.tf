f6f06eb1b06e1388663eb22b872c50b3be5a1b50
florinen <florinen>
2019-03-03 16:10:04 -0600

    edited files

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaleing_group" "dev" {
   name = "dev"
   launch_configuration = "${aws_launch_configuration.dev.name}"
-  min = "${var.min_size}"
-  max = "${var.max_size}"
+  min_size = "${var.min_size}"
+  max_size = "${var.max_size}"
 
 }
